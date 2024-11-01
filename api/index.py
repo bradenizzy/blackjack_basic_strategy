cd4d85cd3962496c4ccb6866bfd3aa4468c9baf2
bradenizzy <braden.israelsen@gmail.com>
2024-10-31 18:41:02 -0600

    removed heroku secret key

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import random
 import os
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key_for_dev')
+#app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key_for_dev')
 
 # Game variables
 CARD_VALUES = {
